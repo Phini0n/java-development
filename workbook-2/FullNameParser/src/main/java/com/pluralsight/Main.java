3fd44804d988bcee461b1e51dab01dc2eb48ee51
Phini0n <patrayowilson@gmail.com>
2024-10-01 13:51:22 -0400

    FullNameParser commit

---
+++ workbook-2/FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String fullName;
+        String firstName;
+        String middleName;
+        String lastName;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        fullName = scanner.nextLine();
+
+        String[] splitName = fullName.split(" ");
+
+        switch (splitName.length) {
+            case 3 :
+                firstName = splitName[0];
+                middleName = splitName[1];
+                lastName = splitName[2];
+                break;
+            case 2 :
+                firstName = splitName[0];
+                middleName = "(none)";
+                lastName = splitName[1];
+                break;
+            case 1 :
+                firstName = splitName[0];
+                middleName = "(none)";
+                lastName = "(none)";
+                break;
+            case 0 :
+                System.out.println("Error: No name was given, exiting");
+                return;
+            default:
+                System.out.println("Error: Maximum amount of names surpassed, exiting.");
+                return;
+        }
+
+        System.out.println("First Name: " + firstName + "\n" +
+                           "Middle Name: " + middleName + "\n" +
+                           "Last Name: " + lastName);
+    }
+}
