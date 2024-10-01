14d882f777e8c1123036e45f2d5120cc77d7d988
Phini0n <patrayowilson@gmail.com>
2024-10-01 10:59:03 -0400

    FullNameApplication commit

---
+++ workbook-2/FullNameApplication/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        // Variables
+        String fullName;
+        String firstName;
+        String middleName;
+        String lastName;
+        String suffix;
+        Scanner scanner = new Scanner(System.in);
+
+        // Name Input
+        System.out.println("Please enter your name");
+        System.out.print("First Name: ");
+        firstName = captureName(scanner.nextLine());
+        System.out.print("Middle name: ");
+        middleName = captureName(scanner.nextLine());
+        System.out.print("Last name: ");
+        lastName = captureName(scanner.nextLine());
+        System.out.print("Suffix: ");
+        suffix = captureName(scanner.nextLine());
+
+        // Ternary to determine if there is a suffix, or not
+        fullName = suffix.isEmpty() ? firstName + middleName + lastName.trim() :
+                firstName + middleName + lastName.trim() + ", " + suffix.trim();
+
+        // Final Output
+        System.out.println("Full name: " +  fullName);
+    }
+
+    // Uses Ternary to determine if there was an input and returns the string trimmed, if so.
+    private static String captureName(String name) {
+        return name.trim().isEmpty() ? "" : name.trim() + " ";
+    }
+}
