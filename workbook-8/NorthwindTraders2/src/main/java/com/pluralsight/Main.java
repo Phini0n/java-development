a1b9ee19d875fc3af6bfd5c06317dd6fedecae61
Phini0n <patrayowilson@gmail.com>
2024-11-26 11:00:20 -0500

    Finished NorthwindTraders3

--- workbook-8/NorthwindTraders2/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders2/src/main/java/com/pluralsight/Main.java
@@ -13,9 +13,13 @@ import javax.sql.DataSource;
 public class Main {
     public static void main(String[] args) {
 
+        ResultSet results = null;
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+
         try {
 
-            if (args.length !=2) {
+            if (args.length != 2) {
                 System.out.println("The application needs a Username and Password to run.");
                 System.exit(1);
             }
@@ -25,58 +29,142 @@ public class Main {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DriverManager.getConnection(
+            connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/northwind",
                     username,
                     password
             );
 
-            String query = """
+            String productQuery = """
                     SELECT productID, productName, unitPrice, unitsInStock
                     FROM products;
                     """;
 
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            String customerQuery = """
+                    SELECT CustomerID, contactName, city, country, phone
+                    FROM customers;
+                    """;
 
-            ResultSet results = preparedStatement.executeQuery(query);
+            // User Input
+            System.out.print("""
+                    What do you want to do?
+                    1) Display all products
+                    2) Display all customers
+                    0) Exit
+                    Select an option: """);
 
-            ArrayList<String> productList = getProductList(results);
+            String query = "";
 
-            for (String product : productList) {
-                System.out.println(product);
+            Scanner scanner = new Scanner(System.in);
+
+            int choice = scanner.nextInt();
+            boolean isRunning = true;
+            while (isRunning) {
+                switch (choice) {
+                    case 1:
+                        query = productQuery;
+                        isRunning = false;
+                        break;
+                    case 2:
+                        query = customerQuery;
+                        isRunning = false;
+                        break;
+                    case 0:
+                        System.exit(1);
+                    default:
+                        System.out.println("Enter the correct number, please.");
+                }
             }
 
-            connection.close();
+
+            preparedStatement = connection.prepareStatement(query);
+
+            results = preparedStatement.executeQuery(query);
+
+            List<String> queryList = new ArrayList<String>();
+
+            if (choice == 1) {
+                queryList = getProductList(results);
+            } else {
+                queryList = getCustomerList(results);
+            }
+
+            for (String s : queryList) {
+                System.out.println(s);
+            }
+
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Error has occurred.");
+            e.printStackTrace();
+
+        } finally {
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public static ArrayList<String> getProductList(ResultSet results) throws SQLException {
-        ArrayList<String> listOfProducts = new ArrayList<String>();
-        int productID, stock;
-        String productName;
-        BigDecimal unitPrice;
+        ArrayList<String> listOfProducts = new ArrayList<>();
 
         while (results.next()) {
-
-            productID = results.getInt("productID");
-            stock = results.getInt("unitsInStock");
-            productName = results.getString("productName");
-            unitPrice = results.getBigDecimal("unitPrice");
-            unitPrice = unitPrice.setScale(2, RoundingMode.CEILING);
-
             listOfProducts.add(String.format(
                     """
                     Product Id: %d
                     Name: %s
-                    Price: %s
+                    Price: %.2f
                     Stock: %d
                     
                     -------------
                     
                     """,
-                    productID, productName, unitPrice, stock
+                    results.getInt("productID"),
+                    results.getString("productName"),
+                    results.getBigDecimal("unitPrice").setScale(2, RoundingMode.CEILING),
+                    results.getInt("unitsInStock")
+            ));
+        }
+        return listOfProducts;
+    }
+
+    public static ArrayList<String> getCustomerList(ResultSet results) throws SQLException {
+        ArrayList<String> listOfProducts = new ArrayList<>();
+        while (results.next()) {
+            listOfProducts.add(String.format(
+                    """
+                    Customer ID: %s
+                    Contact Name: %s
+                    City: %s
+                    Country: %s
+                    Phone: %s
+                    
+                    -------------
+                    
+                    """,
+                    results.getString("CustomerID"),
+                    results.getString("contactName"),
+                    results.getString("city"),
+                    results.getString("country"),
+                    results.getString("phone")
             ));
         }
         return listOfProducts;
